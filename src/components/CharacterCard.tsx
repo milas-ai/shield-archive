86a4c78b495992c89d95e87014f350a3d1fbbeb5
Diogo <diogoribfaria@gmail.com>
2026-05-02 02:16:51 -0300

    refactor: update CharacterCard background style for improved visuals

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -10,19 +10,19 @@ interface Props {
 export const CharacterCard = ({ character, selectedSkinId, onClick }: Props) => {
   const { type, portrait } = getEffectiveStats(character, selectedSkinId);
 
-  const typeColors = {
-    Combat: 'border-red-600',
-    Blast: 'border-blue-600',
-    Speed: 'border-green-600',
-    Universal: 'border-purple-600'
+  const typeGradients = {
+    Combat:    'bg-linear-to-b from-red-800/60 to-slate-950',
+    Blast:     'bg-linear-to-b from-blue-800/60 to-slate-950',
+    Speed:     'bg-linear-to-b from-green-800/60 to-slate-950',
+    Universal: 'bg-linear-to-b from-purple-800/60 to-slate-950',
   };
 
   return (
     <div 
       onClick={onClick}
-      className={`relative w-24 h-24 ... border-2 rounded-xl ${typeColors[type]} ...`}
+      className={`relative w-24 h-24 cursor-pointer overflow-hidden rounded-lg ${typeGradients[type]} transition-transform hover:scale-105 shadow-xl`}
     >
-      <img src={portrait} alt={character.displayName} className='rounded-xl' />
+      <img src={portrait} alt={character.displayName} className='h-full w-full object-cover relative z-10 rounded-lg' />
     </div>
   );
 };
