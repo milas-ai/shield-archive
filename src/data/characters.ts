4e712caed1bfd588d5c1157b043cd0945e56c074
Diogo <diogoribfaria@gmail.com>
2026-05-01 23:03:10 -0300

    feat: add all characters that begin with the letters C and D

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -510,5 +510,467 @@ export const characters: MFFCharacter[] = [
         { id: '2', name: 'Dark Avengers', changedType: 'Speed', changedTags: ['Dark Avengers', 'Weapons Master'] },
         { id: '3', name: 'Wastelanders', changedType: 'Speed' }
     ]
-  }
+  },
+  {
+    id: 'cable',
+    displayName: 'Cable',
+    baseType: 'Blast',
+    baseSide: 'Hero',
+    baseSpecies: 'Mutant',
+    baseGender: 'Male',
+    baseTags: ['Weapons Master', 'Leadership', 'Machine'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'X-Force' },
+        { id: '2', name: 'Cable & Deadpool' },
+        { id: '3', name: 'Summer Days' },
+        { id: '4', name: 'X of Swords' },
+        { id: '5', name: 'Heart of Darkness' }
+    ]
+  },
+  {
+    id: 'captainamerica',
+    displayName: 'Captain America',
+    baseType: 'Combat',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Leadership', 'Command'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'The Avengers' },
+        { id: '1', name: 'Avengers: Age of Ultron' },
+        { id: '2', name: 'Secret Wars: 2099', changedGender: 'Female' },
+        { id: '3', name: 'Marvel Studios Captain America: The Winter Soldier' },
+        { id: '4', name: 'Marvel Studios Captain America: Civil War' },
+        { id: '5', name: 'Marvel NOW!' },
+        { id: '6', name: 'Marvel Studios Avengers: Infinity War' },
+        { id: '7', name: 'Marvel Studios Avengers: Endgame' },
+        { id: '8', name: 'Team Suit' },
+        { id: '10', name: '3099' },
+        { id: '11', name: 'Hydra Supreme', changedType: 'Blast', changedSide: 'Villain', changedTags: ['Command', 'Cosmic Cube'] },
+        { id: '12', name: 'Enter the Phoenix', changedTags: ['Leadership', 'Command', 'Phoenix Force'] },
+        { id: '13', name: 'Back to Basics' },
+        { id: '14', name: 'What If... Zombies?!', changedSide: 'Villain', changedTags: ['Leadership', 'Command', 'Zombie'] },
+        { id: '15', name: 'Galactic Talon' }
+    ]
+  },
+  {
+    id: 'captainmarvel',
+    displayName: 'Captain Marvel',
+    baseType: 'Universal',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Female',
+    baseTags: ['Fast Movement', 'Leadership', 'Energy Projection'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'Secret Wars: Captain Marvel & the Carol Corps' },
+        { id: '2', name: 'Ms. Marvel' },
+        { id: '3', name: 'Marvel Studios Captain Marvel' },
+        { id: '4', name: 'Marvel Studios Avengers: Endgame' },
+        { id: '5', name: 'The Last Avenger', changedSide: 'Villain' },
+        { id: '6', name: 'Marvel Studios The Marvels' },
+        { id: '7', name: 'Marvel Animations Marvel Zombies', changedSide: 'Villain', changedTags: ['Zombie', 'Leadership', 'Energy Projection'] }
+    ]
+  },
+  {
+    id: 'carnage',
+    displayName: 'Carnage',
+    baseType: 'Combat',
+    baseSide: 'Villain',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Evil', 'Healing', 'Symbiote'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'All-New, All-Different' },
+        { id: '1', name: 'Absolute Carnage' },
+        { id: '2', name: 'Fallen Soul' },
+        { id: '3', name: 'Superior Carnage' }
+    ]
+  },
+  {
+    id: 'cassandranova',
+    displayName: 'Cassandra Nova',
+    baseType: 'Blast',
+    baseSide: 'Villain',
+    baseSpecies: 'Mutant',
+    baseGender: 'Female',
+    baseTags: ['Mind', 'Mind Resist', 'Evil'],
+    maxTier: 3,
+    uniforms: [
+        { id: '' , name: 'Marvel Studios Deadpool & Wolverine' }
+    ]
+  },
+  {
+    id: 'cassielang',
+    displayName: 'Cassie Lang',
+    baseType: 'Speed',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Female',
+    baseTags: ['Fast Movement', 'Agility'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Ant-Man and the Wasp: Quantumania' }
+    ]
+  },
+  {
+    id: 'clea',
+    displayName: 'Clea',
+    baseType: 'Universal',
+    baseSide: 'Hero',
+    baseSpecies: 'Alien',
+    baseGender: 'Female',
+    baseTags: ['Magic', 'Fast Movement'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Modern' }
+    ]   
+  },
+  {
+    id: 'colossus',
+    displayName: 'Colossus',
+    baseType: 'Combat',
+    baseSide: 'Hero',
+    baseSpecies: 'Mutant',
+    baseGender: 'Male',
+    baseTags: ['Strong', 'Leaderhip'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'X-Force' },
+        { id: '2', name: 'Phoenix Five', changedSide: 'Villain', changedTags: ['Strong', 'Leadership', 'Phoenix Force'] },
+        { id: '3', name: 'Hellfire Gala' }
+    ]
+  },
+  {
+    id: 'corvusglaive',
+    displayName: 'Corvus Glaive',
+    baseType: 'Universal',
+    baseSide: 'Villain',
+    baseSpecies: 'Alien',
+    baseGender: 'Male',
+    baseTags: ['Agility', 'Fast Movement', 'Black Order'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Infinity' },
+        { id: '1', name: 'Marvel Studios Avengers: Infinity War' },
+        { id: '2', name: 'Dark Obsidian Armor' }
+    ]
+  },
+  {
+    id: 'crescent',
+    displayName: 'Crescent',
+    baseType: 'Combat',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Female',
+    baseTags: ['Leadership', 'Durability', 'Strong'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'Lifestyle Series 1' },
+        { id: '2', name: 'Light Sirius Armor' },
+        { id: '3', name: 'B-Girl Crew' }
+    ]
+  },
+  {
+    id: 'crossbones',
+    displayName: 'Crossbones',
+    baseType: 'Combat',
+    baseSide: 'Villain',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Weapons Master'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Marvel Studios Captain America: Civil War' },
+        { id: '1', name: 'Modern' },
+        { id: '2', name: 'Secret Empire' }
+    ]
+  },
+  {
+    id: 'crystal',
+    displayName: 'Crystal',
+    baseType: 'Blast',
+    baseSide: 'Hero',
+    baseSpecies: 'Inhuman',
+    baseGender: 'Female',
+    baseTags: ['Command', 'Energy Projection'],
+    maxTier: 4,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'All-New, All-Different' },
+        { id: '1', name: 'Royal Suit' },
+        { id: '2', name: 'Fantastic Four', changedTags: ['Command', 'Energy Projection', 'Fantastic Four'] },
+        { id: '3', name: 'Spring Lady' }
+    ]
+  },
+  {
+    id: 'cyclops',
+    displayName: 'Cyclops',
+    baseType: 'Blast',
+    baseSide: 'Hero',
+    baseSpecies: 'Mutant',
+    baseGender: 'Male',
+    baseTags: ['Energy Projection', 'Command'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'Classic' },
+        { id: '1', name: 'Age of Apocalypse', changedSide: 'Villain' },
+        { id: '2', name: 'Marvel NOW!' },
+        { id: '3', name: 'Phoenix Five', changedSide: 'Villain', changedTags: ['Energy Projection', 'Command', 'Phoenix Force'] },
+        { id: '4', name: 'Modern' },
+        { id: '5', name: 'Marvel Animations X-Men 97' }
+    ]
+  },
+  {
+    id: 'daisyjohnson',
+    displayName: 'Daisy Johnson',
+    baseType: 'Blast',
+    baseSide: 'Hero',
+    baseSpecies: 'Inhuman',
+    baseGender: 'Female',
+    baseTags: ['Mind Resist', 'Agent'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Marvel Studios Agents of S.H.I.E.L.D.' },
+        { id: '1', name: 'Modern', changedTags: ['Command', 'Agent'] },
+        { id: '2', name: 'Marvel Studios Agents of S.H.I.E.L.D. (Quake)' }
+    ]
+  },
+  {
+    id: 'daken',
+    displayName: 'Daken',
+    baseType: 'Combat',
+    baseSide: 'Villain',
+    baseSpecies: 'Mutant',
+    baseGender: 'Male',
+    baseTags: ['Weapons Master', 'Healing'],
+    maxTier: 3,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'Dark Wolverine' }
+    ]
+  },
+  {
+    id: 'daredevil',
+    displayName: 'Daredevil',
+    baseType: 'Speed',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Heightened Senses', 'Defenders', 'Leadership'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'Devil of Hells Kitchen' },
+        { id: '2', name: 'All-New, All-Different' },
+        { id: '3', name: 'Fall from Grace' },
+        { id: '4', name: 'Marvel Televisions Daredevil: Born Again' }
+    ]
+  },
+  {
+    id: 'darkhawk',
+    displayName: 'Darkhawk',
+    baseType: 'Speed',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Leaderhip', 'Fast Movement'],
+    maxTier: 2,
+    uniforms: [
+        { id: '' , name: 'Modern' }
+    ]
+  },
+  {
+    id: 'dazzler',
+    displayName: 'Dazzler',
+    baseType: 'Blast',
+    baseSide: 'Hero',
+    baseSpecies: 'Mutant',
+    baseGender: 'Female',
+    baseTags: ['Energy Projection'],
+    maxTier: 4,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'X-Song' }
+    ]
+  },
+  {
+    id: 'deadpool',
+    displayName: 'Deadpool',
+    baseType: 'Speed',
+    baseSide: 'Hero',
+    baseSpecies: 'Mutant',
+    baseGender: 'Male',
+    baseTags: ['Healing', 'Weapons Master'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'X-Force' },
+        { id: '2', name: 'Lady Deadpool', changedGender: 'Female' },
+        { id: '3', name: 'Holiday Party', changedGender: 'Female' },
+        { id: '4', name: '30th Anniversary Black Version' },
+        { id: '5', name: '30th Anniversary White Version' },
+        { id: '6', name: 'April Pools' },
+        { id: '7', name: 'Marvel Studios Deadpool & Wolverine (Nicepool)' },
+        { id: '8', name: 'Marvel Studios Deadpool & Wolverine' }
+    ]
+  },
+  {
+    id: 'deathlok',
+    displayName: 'Deathlok',
+    baseType: 'Combat',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Machine', 'Agent'],
+    maxTier: 2,
+    uniforms: [
+        { id: '' , name: 'Marvel Studios Agents of S.H.I.E.L.D.' },
+        { id: '1', name: 'Modern', changedType: 'Blast' }
+    ]
+  },
+  {
+    id: 'destroyer',
+    displayName: 'Destroyer',
+    baseType: 'Universal',
+    baseSide: 'Neutral',
+    baseSpecies: 'Creature',
+    baseGender: 'None',
+    baseTags: ['Flame', 'Machine'],
+    maxTier: 4,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Classic' },
+        { id: '1', name: 'Prometheus', changedTags: ['Energy Projection', 'Machine'] },
+        { id: '2', name: 'The Mighty Thor', changedTags: ['Energy Projection', 'Machine'] }
+    ]
+  },
+  {
+    id: 'doctordoom',
+    displayName: 'Doctor Doom',
+    baseType: 'Universal',
+    baseSide: 'Villain',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Evil', 'Magic'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: '3099' },
+        { id: '2', name: 'God Emperor' }
+    ]
+  },
+  {
+    id: 'doctoroctopus',
+    displayName: 'Doctor Octopus',
+    baseType: 'Combat',
+    baseSide: 'Villain',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Machine', 'Durability', 'Sinister Six'],
+    maxTier: 4,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Classic' },
+        { id: '1', name: 'Superior Spider-Man', changedSide: 'Hero', changedTags: ['Machine', 'Spider-Sense', 'Agility'] },
+        { id: '2', name: 'Superior Octopus', changedType: 'Speed', changedTags: ['Machine', 'Agility', 'Sinister Six'] },
+        { id: '3', name: 'Spider-Man: No Way Home' },
+        { id: '4', name: 'Ends of the Earth' }
+    ]
+  },
+  {
+    id: 'doctorstrange',
+    displayName: 'Doctor Strange',
+    baseType: 'Blast',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Magic', 'Fast Movement'],
+    maxTier: 4,
+    uniforms: [
+        { id: '' , name: 'All-New, All-Different' },
+        { id: '1', name: 'Marvel Studios Doctor Strange' },
+        { id: '2', name: 'Marvel Studios Avengers: Infinity War' },
+        { id: '3', name: 'Space Suit' },
+        { id: '4', name: 'Marvel Studios Doctor Strange 2' },
+        { id: '5', name: 'Death of Doctor Strange' },
+        { id: '6', name: 'What If... Zombies?!', changedSide: 'Villain', changedTags: ['Magic', 'Fast Movement', 'Zombie'] }
+    ]
+  },
+  {
+    id: 'doctorvoodoo',
+    displayName: 'Doctor Voodoo',
+    baseType: 'Universal',
+    baseSide: 'Hero',
+    baseSpecies: 'Human',
+    baseGender: 'Male',
+    baseTags: ['Magic', 'Command'],
+    maxTier: 4,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Modern' },
+        { id: '1', name: 'Strange Academy' },
+        { id: '2', name: 'Savage Avengers' }
+    ]
+  },
+  {
+    id: 'domino',
+    displayName: 'Domino',
+    baseType: 'Speed',
+    baseSide: 'Hero',
+    baseSpecies: 'Mutant',
+    baseGender: 'Female',
+    baseTags: ['Agility', 'Weapons Master'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'X-Force' },
+        { id: '1', name: 'Marvel NOW!' },
+        { id: '2', name: 'Krakoa X-Force' }
+    ]
+  },
+  {
+    id: 'dormammu',
+    displayName: 'Dormammu',
+    baseType: 'Universal',
+    baseSide: 'Villain',
+    baseSpecies: 'Other',
+    baseGender: 'Male',
+    baseTags: ['Magic', 'Evil'],
+    maxTier: 3,
+    uniforms: [
+        { id: '' , name: 'Classic' },
+        { id: '1', name: 'Damnation' }
+    ]
+  },
+  {
+    id: 'drax',
+    displayName: 'Drax',
+    baseType: 'Combat',
+    baseSide: 'Hero',
+    baseSpecies: 'Alien',
+    baseGender: 'Male',
+    baseTags: ['Healing'],
+    maxTier: 3,
+    transcendent: true,
+    uniforms: [
+        { id: '' , name: 'Guardians of the Galaxy' },
+        { id: '1', name: 'All-New, All-Different' },
+        { id: '2', name: 'Classic' }
+    ]
+  },
 ];
