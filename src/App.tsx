ebf35ff80285288138519940fc5201fb99026296
Diogo <diogoribfaria@gmail.com>
2026-05-04 02:54:56 -0300

    chore: rename application and update related configurations

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div className="flex flex-col h-screen bg-slate-950 text-slate-100">
         
         <header className="p-4 bg-slate-900 border-b border-slate-800 flex gap-8 items-center">
-          <h1 className="text-xl font-bold text-yellow-500">MFF Manager</h1>
+          <h1 className="text-xl font-bold text-yellow-500">SHIELD Archive</h1>
           
           <nav className="flex gap-4">
             <Link to="/" className="hover:text-yellow-500 transition-colors">Characters</Link>
